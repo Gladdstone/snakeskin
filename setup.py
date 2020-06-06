17417ef3a9623ed23e45a840e6cdfe793cc01d14
Joe Farrell <disastrousgamemaster@gmail.com>
2020-06-06 16:52:03 -0400

    added version.txt, created read_from_file function in setup

--- setup.py
+++ setup.py
@@ -2,16 +2,24 @@ import os
 
 from setuptools import setup, find_packages
 
-VERSION = "0.0"
+def read_from_file(file_path, split=False):
+  file_contents = []
+  if os.path.isfile(version_path):
+    with(version_path) as file:
+      if split:
+        file_contents = file.read().splitlines()
+      else:
+        file_contents = file.read()
+  return file_contents
+
+version_path = os.path.dirname(os.path.realpath(__file__)) + "/version.txt"
+VERSION = read_from_file(version_path)
 
 requirements_path = os.path.dirname(os.path.realpath(__file__)) + "/requirements.txt"
-DEPENDENCIES = []
-if os.path.isfile(requirements_path):
-  with open(requirements_path) as file:
-    DEPENDENCIES = file.read().splitlines()
+DEPENDENCIES = read_from_file(requirements_path, split=True)
 
-with open("README.md", "r") as f:
-  LONG_DESCRIPTION = f.read()
+description_path = os.path.dirname(os.path.realpath(__file__)) + "/README.md"
+LONG_DESCRIPTION = read_from_file(description_path)
 
 setup(
   name="snakeskin_cli",
